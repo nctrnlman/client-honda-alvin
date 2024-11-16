776e04e9189e10feada24e2178897cd6245fc34d
Rhazes Devino <rhazesd@gmail.com>
2024-11-16 15:35:45 +0700

    integration with chat bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+"use client";
 import AboutMe from "@/components/AboutMe";
 import Catalog from "@/components/Catalog";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import Promo from "@/components/Promo";
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Menambahkan script Tawk.to ke dalam halaman
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/673856a92480f5b4f59eea7a/1icq152a1"; // Ganti dengan ID Anda
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    // Menyisipkan script ke dalam body
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Menghapus script ketika komponen unmount
+    };
+  }, []);
+
   return (
     <div>
       <Navbar />
